692c9714578c9134ac83ae0c871ab973cc647926
victoriaserranoh <victoriaserranoharo@hotmail.com>
2022-10-07 19:07:07 -0300

    comenzando con el estilo, sigo sin poder solucionar el JS

--- master/appPrincipal.js
+++ master/appPrincipal.js
@@ -1,10 +1,8 @@
+/* ---------- PRIMER SLIDER --- Para cuando le demos click a las flechas hagan un scroll de la fila --------- */
 const line = document.querySelector(".container-slider");
-const movies = document.querySelectorAll(".movies");
-
-const LeftArrow = document.getElementById("left-arrow");
-const RightArrow = document.getElementById("right-arrow");
-
-/* ---------- Event Listener, para cuando le demos click a las flechas hagan un scroll de la fila --------- */
+const movies = document.querySelectorAll(".movies"); 
+const RightArrow = document.getElementById("right-arrow1");
+const LeftArrow = document.getElementById("left-arrow1");
 
 RightArrow.addEventListener("click", () => {
     line.scrollLeft += line.offsetWidth;
@@ -12,9 +10,13 @@ RightArrow.addEventListener("click", () => {
 
 LeftArrow.addEventListener("click", () => {
     line.scrollLeft -= line.offsetWidth;
-});
+});  
 
-/* ----------------- HOVER A LA IMAGEN CUANDO PASO EL CURSOR------------------ */
+/* -------------- SEGUNDO SLIDER ---------- */
+
+
+ /* ----------------- HOVER A LA IMAGEN CUANDO PASO EL CURSOR------------------ */
+
 movies.forEach((movie) => {
     movie.addEventListener("mouseenter", (e) => {
         const element = e.currentTarget;
@@ -25,6 +27,11 @@ movies.forEach((movie) => {
     });
 });
 
+/* Para cuando saque el cursor, se salga el hover de la pelicula */
+line.addEventListener("mouseleave", () => {
+    movies.forEach(movie => movie.classList.remove("hover"));
+})
+
 /* Digo: quiero acceder a todas las peliculas, y agregro un forEach que indique que por cada pelicula que tenga dentro quiero que me 
 ejecute una funcion flecha donde me agregue un EVENTO para obtener el ELEMNTO (por el cual pasamos el cursor), 
 y esto ejecuta otra funcion flecha. Dentro de ella se guarda el elemento en una constante.
@@ -33,7 +40,20 @@ por cada pelicula quiero acceder a pelicula.classlist.remove y le quita la funci
 Luego accedo al elemento por el que pasamor el cursor, y le damos una lista de clase hover*/
 /* EN RESUMEN: es para que cuando pase el cursor a otra pelicula, se le salga el hover a la anterior. */
 
-line.addEventListener("mouseleave", () => {
-    movies.forEach(movie => movie.classList.remove("hover"));
-})
-/* Para cuando saque el cursor, se salga el hover de la pelicula */
+/* -------------------------- BOTON IR ARRIBA ----------------- */
+$(document).ready(function(){
+
+	$('.go-up').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.go-up').slideDown(300);
+		} else {
+			$('.go-up').slideUp(300);
+		}
+	});
+});
