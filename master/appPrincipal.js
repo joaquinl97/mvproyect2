cee5f9a6aba5220a894221b09760bf1b611e348b
victoriaserranoh <victoriaserranoharo@hotmail.com>
2022-10-11 19:47:13 -0300

    avances

--- master/appPrincipal.js
+++ master/appPrincipal.js
@@ -1,18 +1,43 @@
-/* ---------- PRIMER SLIDER --- Para cuando le demos click a las flechas hagan un scroll de la fila --------- */
-const line = document.querySelector(".container-slider");
+/* ---------- PRIMER SLIDER ---------- Para cuando le demos click a las flechas, hagan un scroll de la fila --------- */
 const movies = document.querySelectorAll(".movies"); 
-const RightArrow = document.getElementById("right-arrow1");
-const LeftArrow = document.getElementById("left-arrow1");
 
-RightArrow.addEventListener("click", () => {
-    line.scrollLeft += line.offsetWidth;
+let lineOne = document.getElementById("container-slider1")
+let RightArrowOne = document.getElementById("right-arrow1");		
+let LeftArrowOne = document.getElementById("left-arrow1");
+
+RightArrowOne.addEventListener("click", () => {
+    lineOne.scrollLeft += lineOne.offsetWidth;
 });
 
-LeftArrow.addEventListener("click", () => {
-    line.scrollLeft -= line.offsetWidth;
-});  
+LeftArrowOne.addEventListener("click", () => {
+    lineOne.scrollLeft -= lineOne.offsetWidth;
+});
 
 /* -------------- SEGUNDO SLIDER ---------- */
+let lineTwo = document.getElementById("container-slider2")
+let RightArrowTwo = document.getElementById("right-arrow2");		
+let LeftArrowTwo = document.getElementById("left-arrow2");
+
+RightArrowTwo.addEventListener("click", () => {
+    lineTwo.scrollLeft += lineTwo.offsetWidth;
+});
+
+LeftArrowTwo.addEventListener("click", () => {
+    lineTwo.scrollLeft -= lineTwo.offsetWidth;
+});
+
+/* -------------- TERCER SLIDER ---------- */
+let lineThree = document.getElementById("container-slider3")
+let RightArrowThree = document.getElementById("right-arrow3");		
+let LeftArrowThree = document.getElementById("left-arrow3");
+
+RightArrowThree.addEventListener("click", () => {
+    lineThree.scrollLeft += lineThree.offsetWidth;
+});
+
+LeftArrowThree.addEventListener("click", () => {
+    lineThree.scrollLeft -= lineThree.offsetWidth;
+});
 
 
  /* ----------------- HOVER A LA IMAGEN CUANDO PASO EL CURSOR------------------ */
@@ -28,9 +53,15 @@ movies.forEach((movie) => {
 });
 
 /* Para cuando saque el cursor, se salga el hover de la pelicula */
-line.addEventListener("mouseleave", () => {
-    movies.forEach(movie => movie.classList.remove("hover"));
-})
+lineOne.addEventListener("mouseleave", () => {
+	movies.forEach(movie => movie.classList.remove("hover"));
+}) 
+lineTwo.addEventListener("mouseleave", () => {
+	movies.forEach(movie => movie.classList.remove("hover"));
+}) 
+lineThree.addEventListener("mouseleave", () => {
+	movies.forEach(movie => movie.classList.remove("hover"));
+}) 
 
 /* Digo: quiero acceder a todas las peliculas, y agregro un forEach que indique que por cada pelicula que tenga dentro quiero que me 
 ejecute una funcion flecha donde me agregue un EVENTO para obtener el ELEMNTO (por el cual pasamos el cursor), 
@@ -40,9 +71,8 @@ por cada pelicula quiero acceder a pelicula.classlist.remove y le quita la funci
 Luego accedo al elemento por el que pasamor el cursor, y le damos una lista de clase hover*/
 /* EN RESUMEN: es para que cuando pase el cursor a otra pelicula, se le salga el hover a la anterior. */
 
-/* -------------------------- BOTON IR ARRIBA ----------------- */
+/* -------------------------- BOTON IR HACIA ARRIBA ----------------- */
 $(document).ready(function(){
-
 	$('.go-up').click(function(){
 		$('body, html').animate({
 			scrollTop: '0px'
