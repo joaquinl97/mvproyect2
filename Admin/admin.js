8139aefd5151fe887e9337108a7656f3484592ee
joaquinl97 <gjoaquinlizarraga@hotmail.com>
2022-10-12 08:40:33 -0300

    admin update

--- Admin/admin.js
+++ Admin/admin.js
@@ -1,1 +1,79 @@
-alert('aaaaaa')
+let filmNameAdmin = document.getElementById("filmNameAdmin");
+let optionsAdmin = document.getElementById("optionsAdmin");
+let descriptionAdmin = document.getElementById("descriptionAdmin");
+
+let imageAdmin = document.getElementById("imageAdmin");
+let videoAdmin = document.getElementById("videoAdmin");
+
+let buttonSaveAdmin = document.getElementById("buttonSaveAdmin");
+let viewMoviesAdmin = document.getElementById("viewMoviesAdmin");
+
+let movies =JSON.parse(localStorage.getItem('movies')) || [];
+
+//Create
+buttonSaveAdmin.addEventListener("click", () => {
+    if (filmNameAdmin.value != "" && optionsAdmin.value != "" && descriptionAdmin.value != "") {
+        movies.push({
+            id: Math.round(Math.random() * 1000000),
+            filmNameAdmin:filmNameAdmin.value,
+            optionsAdmin: optionsAdmin.value,
+            descriptionAdmin: descriptionAdmin.value,
+            imageAdmin: imageAdmin.value,
+            videoAdmin: videoAdmin.value,
+        })
+        localStorage.setItem('movies', JSON.stringify(movies));
+        filmNameAdmin.value='';
+        optionsAdmin.value='';
+        descriptionAdmin.value='';
+        imageAdmin.value='';
+        videoAdmin.value='';
+        readFunction()
+
+        window.location.href = "../index.html"
+
+
+    } else{
+        alert('Completar campos')
+    }
+})
+
+//Read
+const readFunction= ()=> {
+    let movies =JSON.parse(localStorage.getItem('movies'))||[];
+    let showMovies=[]
+    movies.map((item)=> {
+        showMovies.push(`
+        <tr class="rowAdmin">
+              <th scope="row">${item.id}</th>
+              <td>${item.filmNameAdmin}</td>
+              <td>${item.optionsAdmin}</td>
+              <td>
+
+                <div class="adminOverflow">
+                  ${item.descriptionAdmin}
+                </div>
+
+              </td>
+              <td>check</td>
+              <td class="editDelFavButtons d-flex">
+                <button class="btn btn-danger p-1 mx-1"><img class="trashImg" src="adminImages/trash-can.png"
+                    alt=""></button>
+                <button class="btn btn-success p-1 mx-1"><img class="trashImg" src="adminImages/edit.png"
+                    alt=""></button>
+                <button class="btn btn-warning p-1 mx-1"><img class="trashImg" src="adminImages/star.png"
+                    alt=""></button>
+
+              </td>
+            </tr>
+        `)
+    })
+    viewMoviesAdmin.innerHTML=showMovies.join('')
+}
+readFunction()
+
+//Update
+let updateFilmNameAdmin
+
+const updateFunction =()=> {
+
+}
---
+++ app.js
@@ -0,0 +1,134 @@
+let filmNameAdmin = document.getElementById("filmNameAdmin");
+let optionsAdmin = document.getElementById("optionsAdmin");
+let descriptionAdmin = document.getElementById("descriptionAdmin");
+
+let imageAdmin = document.getElementById("imageAdmin");
+let videoAdmin = document.getElementById("videoAdmin");
+
+let buttonSaveAdmin = document.getElementById("buttonSaveAdmin");
+let viewMoviesAdmin = document.getElementById("viewMoviesAdmin");
+
+let movies =JSON.parse(localStorage.getItem('movies')) || [];
+
+//Read
+
+function readFunction() {
+    let moviesViewJson = []
+
+    if (movies !== []) {
+        for (let item = 0; item < movies.length; item++) {
+            if (movies[item].optionsAdmin=="Ficcion") {
+                moviesViewJson.push(`
+                <h4>${movies[item].optionsAdmin}</h4>
+                <a href="${movies[item].videoAdmin}">
+                <div class="cardImgMovie m-4">
+                    <div>
+                      <img class="imgMovie" src="${movies[item].imageAdmin}" alt="" srcset="">
+                    </div>
+                    <div class="bg-dark">
+                        <p class="text-light text-center">
+                            <i class="text-light text-center">${movies[item].filmNameAdmin}</i>
+                        </p>
+                    </div>
+                </div>
+                </a>
+                `)    
+            }else if (movies[item].optionsAdmin=="Terror"){
+                moviesViewJson.push(`
+                <h4>${movies[item].optionsAdmin}</h4>
+                <a href="${movies[item].videoAdmin}">
+                <div class="cardImgMovie m-4">
+                    <div>
+                      <img class="imgMovie" src="${movies[item].imageAdmin}" alt="" srcset="">
+                    </div>
+                    <div class="bg-dark">
+                        <p class="text-light text-center">
+                            <i class="text-light text-center">${movies[item].filmNameAdmin}</i>
+                        </p>
+                    </div>
+                </div>
+                </a>
+                `)  
+            }
+            
+        }
+     
+            // let resultFilter2=movies.filter((item)=> item.optionsAdmin=="Terror")
+            // moviesViewJson.push(`
+            // <h4>${resultFilter2}</h4>
+            // <a href="${movies[item].videoAdmin}">
+            // <div class="cardImgMovie m-4">
+            //     <div>
+            //       <img class="imgMovie" src="${movies[item].imageAdmin}" alt="" srcset="">
+            //     </div>
+            //     <div class="bg-dark">
+            //         <p class="text-light text-center">
+            //             <i class="text-light text-center">${movies[item].filmNameAdmin}</i>
+            //         </p>
+            //     </div>
+            // </div>
+            // </a>
+            // `)
+            
+
+            // let resultFilter3=movies.filter((item)=> item.optionsAdmin=="Comedia")
+            // moviesViewJson.push(`
+            // <h4>${resultFilter3}</h4>
+            // <a href="${movies[item].videoAdmin}">
+            // <div class="cardImgMovie m-4">
+            //     <div>
+            //       <img class="imgMovie" src="${movies[item].imageAdmin}" alt="" srcset="">
+            //     </div>
+            //     <div class="bg-dark">
+            //         <p class="text-light text-center">
+            //             <i class="text-light text-center">${movies[item].filmNameAdmin}</i>
+            //         </p>
+            //     </div>
+            // </div>
+            // </a>
+            // `)
+
+            // let resultFilter4=movies.filter((item)=> item.optionsAdmin=="Musical")
+            // moviesViewJson.push(`
+            // <h4>${resultFilter4}</h4>
+            // <a href="${movies[item].videoAdmin}">
+            // <div class="cardImgMovie m-4">
+            //     <div>
+            //       <img class="imgMovie" src="${movies[item].imageAdmin}" alt="" srcset="">
+            //     </div>
+            //     <div class="bg-dark">
+            //         <p class="text-light text-center">
+            //             <i class="text-light text-center">${movies[item].filmNameAdmin}</i>
+            //         </p>
+            //     </div>
+            // </div>
+            // </a>
+            // `)
+
+            // let resultFilter5=movies.filter((item)=> item.optionsAdmin=="Drama")
+            // moviesViewJson.push(`
+            // <h4>${resultFilter5}</h4>
+            // <a href="${movies[item].videoAdmin}">
+            // <div class="cardImgMovie m-4">
+            //     <div>
+            //       <img class="imgMovie" src="${movies[item].imageAdmin}" alt="" srcset="">
+            //     </div>
+            //     <div class="bg-dark">
+            //         <p class="text-light text-center">
+            //             <i class="text-light text-center">${movies[item].filmNameAdmin}</i>
+            //         </p>
+            //     </div>
+            // </div>
+            // </a>
+            // `)
+        
+        
+    } else {
+        swal("Error", "No hay peliculas disponibles", "error");
+    }
+    viewMoviesAdmin.innerHTML = moviesViewJson.join('');
+}
+
+
+
+readFunction()
