2c6d67de08f5cbc12533e3e234b90ba0cca18954
mariamicaelacedron <mikcedron@gmail.com>
2022-10-17 23:24:22 -0300

    cambios para diferenciar entre un usuario y un admi

--- login/login.js
+++ login/login.js
@@ -14,7 +14,7 @@ let loginButton = document.getElementById('loginButton');
 let users = []
 
 //Function  register
-registerButton.addEventListener ('click', () => {
+registerButton.addEventListener('click', () => {
     if (emailRegister.value != "" && passwordRegister.value != "" && nameRegister.value != "") {
         users.push({
             id: Math.round(Math.random() * 1000000),
@@ -26,37 +26,45 @@ registerButton.addEventListener ('click', () => {
         localStorage.setItem("users", JSON.stringify(users));
         swal("Registrado", "Su cuenta fue creada con Exito", "success");
     } else {
-      swal ("Error", "Ingrese sus datos", "error");
+        swal("Error", "Ingrese sus datos", "error");
     }
 })
 
+const admins = [
+    'mica@gmail.com',
+    'alvaro@gmail.com',
+    'joaquin@gmail.com',
+    'augusto@gmail.com',
+    'vicky@gmail.com',
+    'fabrizio@gmail.com',
+
+]
 
 //Function login 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
+    let email = emailLogin.value;
+    let pass = passwordLogin.value;
     if (emailLogin.value != "" && passwordLogin.value != "") {
         let logged = false;
         let response = JSON.parse(localStorage.getItem("users"));
         let userSesion = response.map((item) => {
             if (item.email === emailLogin.value && item.password === passwordLogin.value) {
+                let isAdmin = false;
+
+                if (admins.indexOf(emailLogin.value) >= 0) { //verificar admins
+                    isAdmin = true;
+                }
+
                 let update = {
                     ...item,
                     sesion: true,
+                    isAdmin: isAdmin
                 }
+
                 logged = true;
                 return update;
             } else {
                 return item;
             }
         })
-
-        if (logged) {
-            localStorage.setItem("users", JSON.stringify(userSesion));
-            window.location.href = "/logueado/index.html"
-        } else {
-            swal("Error", "credenciales incorrectas", "error");
-        }
-    } else {
-        swal("Error", "Ingrese sus credenciales", "error");
-    }
-})
